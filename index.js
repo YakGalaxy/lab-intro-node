a1d2ecfd947a16d01fe4b59b5c7fa400f32960d8
alex.laycy <alexlaycy@gmail.com>
2024-04-27 08:38:47 +0100

    Iterations 1 and 2

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 class SortedList {
-  constructor() {}
+  constructor(item, length) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) { return a - b }); 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length -1) {
+      return this.items[pos]; 
+    } else
+      throw new Error("OutOfBounds");
+  }
 
   max() {}
 
