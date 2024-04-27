d713d240b4dd494c7908ea8ed689acb0eed7445b
alex.laycy <alexlaycy@gmail.com>
2024-04-27 08:47:55 +0100

    Iteration 4 done

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 class SortedList {
   constructor(item, length) {
     this.items = [];
-    this.length = this.items.length
+    this.length = this.items.length;
   }
 
   add(item) {
     this.items.push(item);
-    this.items.sort(function (a, b) { return a - b }); 
-    this.length = this.items.length; 
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
   }
 
   get(pos) {
-    if (pos <= this.items.length -1) {
-      return this.items[pos]; 
-    } else
-      throw new Error("OutOfBounds");
+    if (pos <= this.items.length - 1) {
+      return this.items[pos];
+    } else throw new Error("OutOfBounds");
   }
 
-  max() {}
+  max() {
+    if (this.items != "") {
+      this.items.sort(function (a, b) {
+        return b - a;
+      });
+      let highest = this.items[0];
+      return highest;
+    } else throw new Error("EmpySortedList");
+  }
 
   min() {}
 
